fadd6da8aa0f2b376eb64a74ef9b7e698dcef24f
bobrik <ibobrik@gmail.com>
2013-05-13 16:08:30 +0400

    test does not depend on local instances

--- test/test.js
+++ test/test.js
@@ -1,81 +1,109 @@
 (function() {
     var assert     = require("assert"),
         async      = require("async"),
-        Redis      = require("redis"),
+        simple     = require("simple_redis"),
         Queue      = require("zk-redis-queue"),
         Replicator = require("../index.js"),
         ZK         = require("zkjs"),
         zk         = new ZK({hosts: ["api.yongwo.de:2181"], root: "/boo", timeout: 2000}),
-        redisOne   = Redis.createClient(6379, "127.0.0.1", {retry_max_delay: 1000}),
-        redisTwo   = Redis.createClient(6380, "127.0.0.1", {retry_max_delay: 1000}),
-        queue      = new Queue([redisOne, redisTwo], zk, "boo"),
         map        = {1: "http://google.com", 2: "http://yandex.ru"},
-        replicator = new Replicator(queue, map);
+        redisOne, redisTwo, queue;
 
-    replicator.replicate = function(from, to, path, callback) {
-        console.log("replicating", this.map[from] + path, "->", this.map[to] + path);
-        callback();
-    }
+    function createReplicator(callback) {
+        function createRedisOne(callback) {
+            simple.request("ibobrik@gmail.com", {retry_max_delay: 1000}, function(error, redis) {
+                assert.ifError(error);
 
-    queue.on("error", function(error) {
-        console.log("Got queue error", error);
-    });
+                redisOne = redis;
+                callback(error);
+            });
+        }
 
-    replicator.on("ready", function() {
-        console.log("Replicator is ready");
+        function createRedisTwo(callback) {
+            simple.request("ibobrik@gmail.com", {retry_max_delay: 1000}, function(error, redis) {
+                assert.ifError(error);
 
-        async.series([
-            replicator.push.bind(replicator, [1], 2, "/lol"),
-            replicator.push.bind(replicator, [2], 1, "/wtf")
-        ], function(error) {
+                redisTwo = redis;
+                callback();
+            });
+        }
+
+        async.series([createRedisOne, createRedisTwo], function(error) {
             assert.ifError(error);
 
-            replicator.push([2], 3, "/undefined", function(error) {
-                assert.ok(error);
-            });
-
-            replicator.push([3], 2, "/noooo", function(error) {
-                assert.ok(error);
-            });
-
-            queue.getSize(function(error, size) {
-                assert.ifError(error);
-                assert.equal(size, 2);
-
-                console.log('Starting replicator');
-                replicator.start();
-
-                setTimeout(function() {
-                    replicator.stop();
-
-                    replicator.on("stopped", function() {
-                        assert.equal(replicator.started, false);
-
-                        console.log("Replicator stopped");
-
-                        queue.getSize(function(error, size) {
-                            assert.ifError(error);
-                            assert.equal(size, 0);
-
-                            end();
-                        });
-                    });
-                }, 10000);
-            });
+            queue = new Queue([redisOne, redisTwo], zk, "woo");
+            callback(undefined, new Replicator(queue, map));
         });
-    });
+    }
 
-    replicator.on("error", function(error) {
-        console.log("Replicator error occurred", error);
-    });
-
-    function end(error) {
+    createReplicator(function(error, replicator) {
         assert.ifError(error);
 
-        console.log("All done!");
+        replicator.replicate = function(from, to, path, callback) {
+            console.log("replicating", this.map[from] + path, "->", this.map[to] + path);
+            callback();
+        };
 
-        redisOne.quit();
-        redisTwo.quit();
-        zk.close();
-    }
+        queue.on("error", function(error) {
+            console.log("Got queue error", error);
+        });
+
+        replicator.on("ready", function() {
+            console.log("Replicator is ready");
+
+            async.series([
+                replicator.push.bind(replicator, [1], 2, "/lol"),
+                replicator.push.bind(replicator, [2], 1, "/wtf")
+            ], function(error) {
+                assert.ifError(error);
+
+                replicator.push([2], 3, "/undefined", function(error) {
+                    assert.ok(error);
+                });
+
+                replicator.push([3], 2, "/noooo", function(error) {
+                    assert.ok(error);
+                });
+
+                queue.getSize(function(error, size) {
+                    assert.ifError(error);
+                    assert.equal(size, 2);
+
+                    console.log('Starting replicator');
+                    replicator.start();
+
+                    setTimeout(function() {
+                        replicator.stop();
+
+                        replicator.on("stopped", function() {
+                            assert.equal(replicator.started, false);
+
+                            console.log("Replicator stopped");
+
+                            queue.getSize(function(error, size) {
+                                assert.ifError(error);
+                                assert.equal(size, 0);
+
+                                end();
+                            });
+                        });
+                    }, 10000);
+                });
+            });
+        });
+
+        replicator.on("error", function(error) {
+            console.log("Replicator error occurred", error);
+        });
+
+        function end(error) {
+            assert.ifError(error);
+
+            console.log("All done!");
+
+            redisOne.quit();
+            redisTwo.quit();
+            zk.close();
+        }
+    });
 })();
